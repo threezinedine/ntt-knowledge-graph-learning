822938552cb119653686acc683227acf10199fa4
threezinedine <threezinedine@gmail.com>
2025-06-30 21:05:32 +0700

    [configure] add reloader for UI dev development

--- app/loader/index.ts
+++ app/loader/index.ts
@@ -1,27 +1,30 @@
 import { app, BrowserWindow } from 'electron';
+import reloader from 'electron-reloader';
+
+reloader(module);
 
 function createWindow() {
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-        },
-    });
+	const mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	});
 
-    mainWindow.loadFile('index.html');
+	mainWindow.loadFile('index.html');
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
 app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
 });
--- app/ui/app.tsx
+++ app/ui/app.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Application(): React.ReactElement {
 	return (
 		<div>
-			<h1>Hello World 2</h1>
+			<h1>Hello World 4</h1>
 		</div>
 	);
 }
